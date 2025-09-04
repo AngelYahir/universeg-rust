dde988ac2f2b335666a505c202de8053566df0ec
AngelYahir <hanyo27orres@gmail.com>
2025-09-03 18:43:02 -0600

    Feat: Added swagger docs

--- src/interface/rest/routes/mod.rs
+++ src/interface/rest/routes/mod.rs
@@ -1,8 +1,38 @@
 use axum::{Router, routing::get};
+use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
 
 pub mod auth;
 pub mod user;
 
+#[utoipa::path(
+    get,
+    path = "/",
+    tag = "root",
+    summary = "Health check",
+    description = "Health check endpoint",
+    responses(
+        (status = 200, description = "Hello there"),
+        (status = 400, description = "Error")
+    )
+)]
+async fn health() -> &'static str {
+    "/"
+}
+
+#[derive(OpenApi)]
+#[openapi(
+    paths(
+        health,
+    ),
+    tags(
+        (name = "root", description = "Root route"),
+    )
+)]
+struct ApiDoc;
+
 pub fn routes() -> Router<()> {
-    Router::new().route("/", get(|| async { "Hello there" }))
+    Router::new()
+        .route("/", get(|| async { "Hello there" }))
+        .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()))
 }
